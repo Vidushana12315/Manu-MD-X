88b6b3310dd984441c568fb32b4720fdb591d8b0
Vidushana12315 <vidushana.bandara@gmail.com>
2024-12-16 11:19:55 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&vcUAUbzQ#mGjdtH1fppDX7kwBZvLuO-hR21uCX5FoGGOCBgo2iHA",
 };
